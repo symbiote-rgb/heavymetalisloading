dcd64a09a605b41dbcf5cc7196d05c4bf5923189
cromulentjuice <78133191+cromulentjuice@users.noreply.github.com>
2025-04-16 20:35:40 -0400

    Create crt-overlay.tsx

---
+++ components/crt-overlay.tsx
@@ -0,0 +1,9 @@
+export default function CRTOverlay() {
+  return (
+    <div className="crt-overlay">
+      <div className="crt-scanlines"></div>
+      <div className="crt-flicker"></div>
+      <div className="crt-vignette"></div>
+    </div>
+  )
+}
