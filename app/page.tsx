a9cdfe80ea017d75a80086006e4523595a00319c
symbiote-rgb <edynhillman@gmail.com>
2025-07-22 16:41:56 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main
       className="min-h-screen w-full flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat overflow-hidden scrolling-background"
       style={{
-        backgroundImage: "url(/images/background.png)",
+        backgroundImage: "url(/images/0005.png)",
         backgroundSize: "200% 200%",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
